5f81850e9b701bf8e9055a60726cd35412194367
Johan Borgenstierna <johan.bx@yubico.com>
2020-12-02 21:55:17 +0100

    run all capabilities

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-if (process.argv.length !== 4) {
-  console.log("EXAMPLE: node index.js 1a 1.txt");
-  return;
+const { fstat } = require("fs");
+
+const fs = require("fs");
+
+const run = (script, puzzle) => {
+  process.env.PUZZLE = require("fs").readFileSync(
+    "./inputs/" + puzzle + ".txt"
+  );
+  process.stdout.write(script + ": ");
+  const start = process.hrtime();
+  require("./days/" + script);
+  const hrend = process.hrtime(start);
+  console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1] / 1000000);
+};
+
+if (process.argv[2] === "all") {
+  fs.readdirSync("./days").map((name) => {
+    run(name, name.slice(0, -4));
+  });
+} else if (process.argv.length !== 4) {
+  console.log(
+    "USAGE node index.js [day] [input]\nEXAMPLE: node index.js 2b 2\nALL: node index.js all"
+  );
+} else {
+  run(process.argv[2], process.argv[3]);
 }
-process.env.PUZZLE = require("fs").readFileSync("./inputs/" + process.argv[3]);
-process.stdout.write(process.argv[2] + ": ");
-const start = process.hrtime();
-require("./days/" + process.argv[2]);
-const hrend = process.hrtime(start);
-console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1] / 1000000);
