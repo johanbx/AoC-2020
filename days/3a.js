fe83a3b6e08131a63a1701991388276bf4285572
Johan Borgenstierna <johan.bx@yubico.com>
2020-12-03 10:34:59 +0100

    day 3a

---
+++ days/3a.js
@@ -0,0 +1,15 @@
+const { stdout } = require("process");
+
+const list = process.env.PUZZLE.split("\n");
+
+const answer = () => {
+  let trees = 0;
+  for (let y = 1; y < list.length; y += 1) {
+    if (list[y][(y * 3) % list[0].length] === "#") {
+      trees += 1;
+    }
+  }
+  return trees;
+};
+
+console.log(answer());
